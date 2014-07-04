899d28adb20ab55752393e6ff7ef0ca512a39027
James Reeves <jreeves@weavejester.com>
2014-07-04 19:43:09 +0100

    Add basic file watch

--- src/leiningen/auto.clj
+++ src/leiningen/auto.clj
@@ -1,6 +1,22 @@
-(ns leiningen.auto)
+(ns leiningen.auto
+  (:require [clojure.java.io :as io])
+  (:import (java.io File)))
 
-(defn auto
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+(defn project-files [project]
+  (file-seq (io/file (:root project))))
+
+(defn modified-since [^File file timestamp]
+  (> (.lastModified file) timestamp))
+
+(defn modified-files [project timestamp]
+  (->> (project-files project)
+       (remove #(.isDirectory ^File %))
+       (filter #(modified-since % timestamp))))
+
+(defn auto [project & args]
+  (loop [time 0]
+    (Thread/sleep 100)
+    (if-let [files (seq (modified-files project time))]
+      (do (prn files)
+          (recur (System/currentTimeMillis)))
+      (recur time))))
