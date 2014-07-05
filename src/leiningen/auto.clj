d23fbec43d8777b76bacc7eca67c70fe7d4bbb3f
James Reeves <jreeves@weavejester.com>
2014-07-05 10:34:27 +0100

    Add configurable colors to output

--- src/leiningen/auto.clj
+++ src/leiningen/auto.clj
@@ -21,16 +21,35 @@
 
 (def default-file-pattern #"\.(clj|cljs|cljx)$")
 
-(defn log [& strs]
-  (println (str "auto> " (str/join " " strs))))
+(def ansi-codes
+  {:reset   "\u001b[0m"
+   :black   "\u001b[30m" :gray           "\u001b[1m\u001b[30m"
+   :red     "\u001b[31m" :bright-red     "\u001b[1m\u001b[31m"
+   :green   "\u001b[32m" :bright-green   "\u001b[1m\u001b[32m"
+   :yellow  "\u001b[33m" :bright-yellow  "\u001b[1m\u001b[33m"
+   :blue    "\u001b[34m" :bright-blue    "\u001b[1m\u001b[34m"
+   :magenta "\u001b[35m" :bright-magenta "\u001b[1m\u001b[35m"
+   :cyan    "\u001b[36m" :bright-cyan    "\u001b[1m\u001b[36m"
+   :white   "\u001b[37m" :bright-white   "\u001b[1m\u001b[37m"
+   :default "\u001b[39m"})
+
+(defn ansi [code & [default]]
+  (str (ansi-codes code (ansi-codes default))))
+
+(defn log [project & strs]
+  (let [color (get-in project [:auto :color] true)
+        text  (str/join " " strs)]
+    (if color
+      (println (str (ansi color :magenta) "auto> " text (ansi :reset)))
+      (println (str "auto> " text)))))
 
 (defn run-task [project task args]
-  (log "Running: lein" task (str/join " " args))
+  (log project "Running: lein" task (str/join " " args))
   (binding [main/*exit-process?* false]
     (try
       (main/resolve-and-apply project (cons task args))
       (catch ExceptionInfo _)))
-  (log "Done."))
+  (log project "Done."))
 
 (defn add-ending-separator [^String path]
   (if (.endsWith path File/separator)
@@ -45,7 +64,7 @@
 (defn show-modified [project files]
   (let [root  (add-ending-separator (:root project))
         paths (map #(remove-prefix (str %) root) files)]
-    (log "Files changed:" (str/join ", " paths))))
+    (log project "Files changed:" (str/join ", " paths))))
 
 (defn auto
   "Executes the given task every time a file in the project is modified."
