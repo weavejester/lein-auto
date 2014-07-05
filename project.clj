179b9b644275d906b56475b2c717ce9829fd8258
James Reeves <jreeves@weavejester.com>
2014-07-05 16:49:09 +0100

    Add description and URL to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-auto "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plugin that executes tasks when files are modified"
+  :url "https://github.com/weavejester/lein-auto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
