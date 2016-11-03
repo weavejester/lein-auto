43eb3b85224f52d7507b3ad52a906ee01d3e3619
James Reeves <jreeves@weavejester.com>
2016-11-03 19:58:35 +0000

    Release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-auto "0.1.2"
+(defproject lein-auto "0.1.3"
   :description "Leiningen plugin that executes tasks when files are modified"
   :url "https://github.com/weavejester/lein-auto"
   :license {:name "Eclipse Public License"
